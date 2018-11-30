222c90757305c5f8c2904e42568f6f22ee697b2a
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-11-30 14:52:28 +0100

    cleanup

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ if (!config || !config.port || !config.endpoint) process.exit();
 
 // Initialize Metrics Server
 console.log('Initializing Metrics...');
-const metrics = require('./metrics').metrics;
+const metrics = require('./lib/metrics').metrics;
 metrics.start(config.port,config.host,config.endpoint);
 
 console.log('Initializing Socket...',config.interface,config.bpf);
-const pcap = require('./pcap').pcap;
+const pcap = require('./lib/pcap').pcap;
 pcap.start(config.interface,config.bpf);
 
 console.log('Running...');
